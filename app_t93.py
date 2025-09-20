a80b82ddea6cc3c041246b29c44c1570c8cff24b
BlackBow393 <t9374bow86@outlook.com>
2025-09-20 21:58:20 +0900

    フォルダ選択機能の試作 設定画面の作成

--- app_t93.py
+++ app_t93.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request, jsonify
 from dash_app.routes import routes_bp
 from dash_app.dashbord_1 import create_dash_app
 import os
@@ -13,6 +14,24 @@ create_dash_app(app)
 @app.route('/')
 def home():
     return render_template('main.html')
+
+@app.route('/asset')
+def asset():
+    return render_template('asset.html')
+
+@app.route('/setting')
+def setting():
+    return render_template('setting.html')
+
+@app.route("/get_fullpath", methods=["POST"])
+def get_fullpath():
+    data = request.json
+    relative_path = data.get("relativePath", "")
+
+    # Flaskの実行ディレクトリから見たフルパスを作成
+    full_path = os.path.abspath(relative_path)
+
+    return jsonify({"fullPath": full_path})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
